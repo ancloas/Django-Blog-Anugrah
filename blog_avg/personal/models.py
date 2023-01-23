256b8f1b505726a6af5c943d6fde564d4fe6e0ce
Your Name <you@example.com>
2023-01-23 21:19:18 +0530

    second commit

--- blog_avg/personal/models.py
+++ blog_avg/personal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+Priority=[ ("L","Low"), ("M", "Medium"), ("H", "High") ]
+
+class Question(models.Model):
+    title= models.CharField(max_length=60)
+    question= models.TextField(max_length=400)
+    priority=models.CharField(max_length=1, choices=Priority)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name="The Question"
+        verbose_name_plural="People Questions"
--- blog_avg/personal/views.py
+++ blog_avg/personal/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from personal.models  import Question
 # Create your views here.
 def home_screen_view(request):
-    print(request.headers)
-    return render(request, 'base.html',{})
+    context={}
+    context['some_string']= "this is some string that i am passing to view"
+    
+    question_list=Question.objects.all();
+    context['question_list']=question_list
+
+
+    return render(request, 'personal/home.html',context)
