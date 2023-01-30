58b7e4318e746e335fd1b0e6041a9f5bb3c731f5
Your Name <you@example.com>
2023-01-30 23:29:24 +0530

    added registration page

--- blog_avg/blog_avg/urls.py
+++ blog_avg/blog_avg/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from personal.views import (home_screen_view,)
+from account.views import (registration_view,)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name='home'),
+    path('register/', registration_view, name="register")
     
 ]
