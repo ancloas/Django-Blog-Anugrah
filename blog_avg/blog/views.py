d4f066ecbbc686aee4844edce2c44cb401b92e53
Anugrah <anugrahgupta.52@gmail.com>
2023-03-24 21:38:17 +0530

    pagination

--- blog_avg/blog/views.py
+++ blog_avg/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
+from django.http import HttpResponse
 
 from blog.models import BlogPost
 from blog.forms import CreateBlogPostForm, EditBlogPostForm
@@ -44,6 +45,10 @@ def edit_blog_view(request, slug):
 		return redirect('must_authenticate')
 
 	blog_post = get_object_or_404(BlogPost, slug=slug)
+
+	if blog_post.author != user:
+		return HttpResponse("You are not the author of the post")
+
 	if request.POST:
 		form = EditBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
 		if form.is_valid():
--- blog_avg/personal/views.py
+++ blog_avg/personal/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from operator import attrgetter
-from blog.models import BlogPost
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from blog.views import get_blog_queryset
 
 
 # Create your views here.
-
+BLOG_POSTS_PER_PAGE=10
 
 def home_screen_view(request):
     context={}
@@ -13,10 +14,26 @@ def home_screen_view(request):
     query =""
     
     if request.GET:
-        query=request.GET['q']
+        query=request.GET.get('q','')
         context['query'] = str(query)
 
 
     blog_posts = sorted(get_blog_queryset(query), key= attrgetter('date_updated'), reverse=True)
+
+
+    #   Pagination
+    page = request.GET.get('page', 1)
+    blog_posts_paginator=Paginator(blog_posts, BLOG_POSTS_PER_PAGE)
+
+    try:
+        blog_posts = blog_posts_paginator.page(page)
+    except PageNotAnInteger:
+        blog_posts = blog_posts_paginator.page(BLOG_POSTS_PER_PAGE)
+    except EmptyPage:
+        blog_posts = blog_posts_paginator.page(blog_posts_paginator.num_pages)
+        
     context['blog_posts']=blog_posts
+
+
+
     return render(request, 'personal/home.html',context)
