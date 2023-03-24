c54905efe08aea1c840f77a526158b07f693f009
Anugrah <anugrahgupta.52@gmail.com>
2023-03-24 16:41:24 +0530

    added the blog feed functionality

--- blog_avg/personal/views.py
+++ blog_avg/personal/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from account.models import Account
+from operator import attrgetter
+from blog.models import BlogPost
+
+
 # Create your views here.
+
+
 def home_screen_view(request):
     context={}
-    context['some_string']= "this is some string that i am passing to view"
-    accounts=Account.objects.all();
-    context['account_list']=accounts
-
-
+    blog_posts = sorted(BlogPost.objects.all(), key= attrgetter('date_updated'), reverse=True)
+    context['blog_posts']=blog_posts
     return render(request, 'personal/home.html',context)
