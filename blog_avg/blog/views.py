3ed48690b65ebf3d389eb1271ad79d5437a4082c
Anugrah <anugrahgupta.52@gmail.com>
2023-03-24 20:52:29 +0530

    django q lookup search bar.

--- blog_avg/blog/views.py
+++ blog_avg/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 
 from blog.models import BlogPost
 from blog.forms import CreateBlogPostForm, EditBlogPostForm
@@ -61,3 +62,17 @@ def edit_blog_view(request, slug):
 	context['form'] = form
 
 	return render(request, 'blog/edit_blog.html', context)
+
+
+def get_blog_queryset(query=None):
+	queryset= []
+	queries= query.split(" ")  
+	for q in queries: 
+		posts= BlogPost.objects.filter(Q(title__icontains=q) | Q(body__icontains=q)).distinct()
+
+		for post in posts:
+			queryset.append(post)
+
+	return list(set(queryset))
+	
+      
