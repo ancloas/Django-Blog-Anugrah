9c14680cde1bee3b92d7959cd402ae5b072556fe
Anugrah <anugrahgupta.52@gmail.com>
2023-12-07 04:09:52 +0530

    updated deploy command to create superuser

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,11 @@ python blog_avg/manage.py makemigrations
 # Run migrate
 python blog_avg/manage.py migrate
 
+# Add super user
+if [[ $CREATE_SUPERUSER ]];
+then
+  python blog_avg/manage.py createsuperuser --no-input
+fi
+
 # Run runserver
 python blog_avg/manage.py runserver 0.0.0.0:8000
