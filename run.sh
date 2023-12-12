05949813ab8d1a3387915de0729f1568e53543df
Anugrah <anugrahgupta.52@gmail.com>
2023-12-13 01:05:05 +0530

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-
-# Run runserver
+# Run Gunicorn with relative path to the parent directory
 cd blog_avg
-gunicorn blog_avg.wsgi:application -c gunicorn_config.py
+gunicorn blog_avg.wsgi:application -c ../gunicorn_config.py
